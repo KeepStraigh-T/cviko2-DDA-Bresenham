9a183c504c34456e320f6578bfdc60596a188546
Yaroslav <gavadaasd@gmail.com>
2026-02-25 08:18:50 +0100

    Add project files.

---
+++ build/.qt/QtDeployTargets-Debug.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_ImageViewer_FILE Q:/Qt-projects/PG-Cviko2-DDA-Bresenham/build/Debug/ImageViewer.exe)
+set(__QT_DEPLOY_TARGET_ImageViewer_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_ImageViewer_RUNTIME_DLLS C:/Qt/6.10.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Cored.dll)
---
+++ build/.qt/QtDeployTargets-RelWithDebInfo.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_ImageViewer_FILE Q:/Qt-projects/PG-Cviko2-DDA-Bresenham/build/RelWithDebInfo/ImageViewer.exe)
+set(__QT_DEPLOY_TARGET_ImageViewer_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_ImageViewer_RUNTIME_DLLS C:/Qt/6.10.1/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Core.dll)
