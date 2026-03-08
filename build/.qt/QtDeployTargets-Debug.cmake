6d213b47716cb6b3ef8fc58c34aa728abacc0acf
Yaroslav <gavadaasd@gmail.com>
2026-03-08 17:20:11 +0100

    Delete build dir

--- build/.qt/QtDeployTargets-Debug.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ImageViewer_FILE D:/Projects/Yaroslav-Kepko/PG-cvik3/KeepStraigh-T/cviko2-DDA-Bresenham/build/Debug/ImageViewer.exe)
-set(__QT_DEPLOY_TARGET_ImageViewer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ImageViewer_RUNTIME_DLLS C:/Qt/6.10.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Cored.dll)
--- build/.qt/QtDeployTargets-MinSizeRel.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ImageViewer_FILE D:/Projects/Yaroslav-Kepko/PG-cvik3/KeepStraigh-T/cviko2-DDA-Bresenham/build/MinSizeRel/ImageViewer.exe)
-set(__QT_DEPLOY_TARGET_ImageViewer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ImageViewer_RUNTIME_DLLS C:/Qt/6.10.1/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Core.dll)
